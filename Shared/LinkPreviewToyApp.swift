140e3312cf43cf7dd173799d4429c4a417f14da7
Jaanus Kase <jaanus@gmail.com>
2022-01-05 14:08:29 +0200

    Initial Commit

---
+++ Shared/LinkPreviewToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkPreviewToyApp.swift
+//  Shared
+//
+//  Created by Jaanus Kase on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct LinkPreviewToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
