1bf7458b8a1eab8c27bb7781a65c09f2d101b4fd
Jaanus Kase <jaanus@gmail.com>
2022-01-06 09:48:40 +0200

    fix up provider creation

--- Shared/Model/Model.swift
+++ Shared/Model/Model.swift
@@ -7,12 +7,16 @@
 
 import Combine
 import Foundation
+import LinkPresentation
 
 class Model: ObservableObject {
     
     @Published private(set) var state = UIState.idle
-    
+        
     func startLoadingURL(_ url: URL) {
-        state = .loading
+        state = .loading(url)
+        LPMetadataProvider().startFetchingMetadata(for: url) { metadata, error in
+            print("Fetched. Metadata: \(String(describing: metadata)), error: \(String(describing: error))")
+        }
     }
 }
