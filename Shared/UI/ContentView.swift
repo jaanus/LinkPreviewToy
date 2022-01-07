d9734ccf074e906b1415ff28bba1522b0bfb88b0
Jaanus Kase <jaanus@gmail.com>
2022-01-07 22:36:13 +0200

    Gate the preview with debug

--- Shared/UI/ContentView.swift
+++ Shared/UI/ContentView.swift
@@ -43,6 +43,7 @@ struct ContentView: View {
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         
@@ -59,3 +60,4 @@ struct ContentView_Previews: PreviewProvider {
         }
     }
 }
+#endif
